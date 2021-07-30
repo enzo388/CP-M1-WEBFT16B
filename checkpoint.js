71b080c7fd600d665f13bcc291c050d7a0854af1
enzo388 <enzo.vazquez.388@gmail.com>
2021-07-30 17:35:33 -0300

    Chekpoint terminado

--- checkpoint.js
+++ checkpoint.js
@@ -92,6 +92,7 @@ function secuenciaHenry(obj, n) {
   // Tu código aca:
    
   if (n < 0) return null;
+  
   if (n === 0) return obj.first;
   if (n === 1) return Object.keys(obj).length;
   return (secuenciaHenry(obj, n - 1) * secuenciaHenry(obj, n - 2) - secuenciaHenry(obj, n - 2));
